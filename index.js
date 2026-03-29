29107a9c6f60f1d14924f28be895c3b408bc5de4
saeznahuel013-max <saeznahuel013@gmail.com>
2026-03-29 21:53:58 +0200

    index.js

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
-// Esto le dice al FIFA que el servidor está OK
+// Esto engaña al FIFA para que crea que los servidores están activos
 app.get('*', (req, res) => {
-  console.log("FIFA intentando conectar...");
-  res.send('EA_GATEWAY_OK');
+  console.log("FIFA preguntando por disponibilidad...");
+  // Estas son las respuestas que el juego busca para dejarte pasar
+  res.send('AVAILABLE=1\nMAINTENANCE=0\nIS_ONLINE=1');
+});
+
+app.post('*', (req, res) => {
+  console.log("FIFA intentando loguear...");
+  res.send('SUCCESS=1');
 });
 
 app.listen(port, () => {
-  console.log(`Servidor de FIFA corriendo en puerto ${port}`);
+  console.log(`Servidor Redirector FIFA activo en puerto ${port}`);
 });
